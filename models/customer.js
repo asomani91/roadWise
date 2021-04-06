96a74f9a9c4bbb03a48466ed0655a96210b4f9f9
rashawn doyley <rashawndoyley@gmail.com>
2021-04-06 13:59:25 -0400

    new structure

---
+++ models/customer.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const customerSchema = new Schema({
+    name: { type: String, required: true },
+
+})
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+module.exports = Customer;
