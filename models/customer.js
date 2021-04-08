b657378ba9f8f4add0a2026dab686a0dffe10b73
rashawn doyley <rashawndoyley@gmail.com>
2021-04-08 12:04:23 -0400

    new update

---
+++ models/customer.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const customerSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, },
+    passowrd: { type: String, required: true },
+})
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+module.exports = Customer;
