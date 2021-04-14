804c2364c8b8cdd54fecc312bc61dfb248fe09d2
Hiral patel <hiralm227@gmail.com>
2021-04-14 13:09:53 -0400

    package.json fixed

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
+
 // Add routes, both API and view
 app.use(routes);
 
