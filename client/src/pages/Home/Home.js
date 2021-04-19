fa5cebc964ac36e6720500bb6cca49d4a86da486
rashawn doyley <rashawndoyley@gmail.com>
2021-04-19 07:09:27 -0700

    new update

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,23 +1,17 @@
 import React from "react";
-import {Jumbotron,Button} from "reactstrap";
-import "../Home/home.css"
-import Contact from "../Contact/Contact"
+import { Jumbotron, Button } from "reactstrap";
+import "../Home/home.css";
+import Contact from "../Contact/Contact";
 import Cards from "../../components/cards";
-import Gif from "../../components/Gif/Gif"
+import Gif from "../../components/Gif/Gif";
 const Home = () => (
-  <div >
-      <Jumbotron id="jumbo" >
-        <h1 className="display-3">Road Wise </h1>
-        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr className="my-2" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p className="lead">
-          {/* <Button color="primary">Learn More</Button> */}
-        </p>
-      </Jumbotron>
-      <Cards/>
-      <Contact />
-    </div>
+  <div>
+    <Jumbotron id="jumbo">
+      <h1 className="display-3"> Road Wise </h1>
+    </Jumbotron>
+    <Cards />
+    <Contact />
+  </div>
 );
 
 export default Home;
